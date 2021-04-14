6668b6f7fd94d05b9406547a20f9151b7714cde4
Inposa <donhippo04@gmail.com>
2021-04-14 15:04:36 +0200

    add some console.log band aid for quick debug

--- inposa-linkshortener/pages/api/_connector.ts
+++ inposa-linkshortener/pages/api/_connector.ts
@@ -6,6 +6,7 @@ export async function connectToDatabase() {
     if(cacheDb){
         return cacheDb;
     }
+    console.log("connexion à la bdd...")
     const client = new MongoClient(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
     cacheDb = client; 
--- inposa-linkshortener/pages/api/hello.ts
+++ inposa-linkshortener/pages/api/hello.ts
@@ -3,6 +3,7 @@
 import {connectToDatabase} from "./_connector";
 
 export default async (req, res) => {
+  console.log("hello world !")
   await connectToDatabase();
   
   res.status(200).json({ name: 'John Doe' })
