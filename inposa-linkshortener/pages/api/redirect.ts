a72d14fdfe91e5c533e216dfe40239d1489f3363
Inposa <donhippo04@gmail.com>
2021-04-14 16:32:05 +0200

    fix redirect

--- inposa-linkshortener/pages/api/redirect.ts
+++ inposa-linkshortener/pages/api/redirect.ts
@@ -5,9 +5,10 @@ export default async (req, res) => {
   const db = await connectToDatabase();
 
   const entry = await db
-    .db("link_db")
+    .db("links_db")
     .collection("links_collection")
     .findOne({ _id: new ObjectID(req.query.id as string) });
+
   if (entry !== null) {
     return res.redirect(301, entry.link);
   }
