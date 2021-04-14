09d41bb4c25516afecf43aca330ded742d3dd17d
Inposa <donhippo04@gmail.com>
2021-04-14 15:35:00 +0200

    fix the shorten link

--- inposa-linkshortener/pages/api/shorten_link.ts
+++ inposa-linkshortener/pages/api/shorten_link.ts
@@ -1,17 +1,36 @@
-import { connectToDatabase } from "./_connector";
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { MongoClient } from "mongodb";
+import { NowRequest, NowResponse } from "@vercel/node";
 
-export default async (req, res) => {
+let cachedDb;
+
+async function connectToDatabase() {
+  if (cachedDb) {
+    return cachedDb;
+  }
+  const client = new MongoClient(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+  });
+
+  cachedDb = client;
+  return await client.connect();
+}
+
+export default async (req: NowRequest, res: NowResponse) => {
   const db = await connectToDatabase();
 
-  if (req.body !== "" && req.body.link !== undefined && req.body.link !== "") {
+  if (req.body.link) {
     const entry = await db
-      .db("link_db")
-      .collection("link_collection")
+      .db("links_db")
+      .collection("links_collection")
       .insertOne({ link: req.body.link });
-    res.statusCode = 201;
-    return res.json({short_link: `${process.env.VERCEL_URL}/r/${entry.insertedId}`});
+
+    res.statusCode = 201; // created
+    return res.json({
+      short_link: `${process.env.VERCEL_URL}/r/${entry.insertedId}`,
+    });
   }
 
-  res.statusCode = 409;
-  res.json({error: 'no_link_found', error_description:'No link found !'});
+  res.statusCode = 409; // conflict
+  res.json({ error: "no_link_found", error_description: "No link found" });
 };
